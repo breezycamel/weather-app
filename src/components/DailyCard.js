940448b6e9692455e9755e9011ad339fc59ef9ed
Phuoc Ho <phuocho99@gmail.com>
2020-08-27 17:27:28 -0400

    built dailyPanel

--- src/components/DailyCard.js
+++ src/components/DailyCard.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import {convertKelvinToF} from '../util/util.js';
 import moment from 'moment'
 
-export default function DailyCard({data, index}) {
-	console.log(data);
+export default function DailyCard({data, isSelected, onClick}) {
 	const weatherImageURL = 'http://openweathermap.org/img/wn/';
 	const imageSize = '@2x.png';
-	const day = moment.parseZone(data.dt);
+	const day = moment.parseZone(data.dt*1000);
+	const minTemp = convertKelvinToF(data.temp.min);
+	const maxTemp = convertKelvinToF(data.temp.max);
 	return (
-		<div>
-			<p>{day.format('ddd')}</p>
-			<img src={weatherImageURL+data.weather[0].icon+imageSize}/>
+		<div className={(isSelected)? 'selected-daily-card':'daily-card'} onClick={onClick}>
+			<p className='daily-card-day'>{day.format('ddd')}</p>
+			<img className='daily-card-image' src={weatherImageURL+data.weather[0].icon+imageSize}/>
+			<p className='daily-card-temperature'>{maxTemp.toFixed(0)+'\u00B0'+' '+minTemp.toFixed(0)+'\u00B0'}</p>
 		</div>
 	);
 }
--- src/components/DailyPanel.js
+++ src/components/DailyPanel.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState}from 'react';
 import DailyCard from './DailyCard.js';
 import {meterPerSecToMPH, convertKelvinToF} from '../util/util.js';
 import moment from 'moment';
 
 export default function CurrentPanel({daily, gotDaily}) {
+	const [curIndex, setCurIndex] = useState(0);
 	if(!gotDaily){
 		return(
 			<div className='daily-panel'>
@@ -11,21 +12,45 @@ export default function CurrentPanel({daily, gotDaily}) {
 		)
 	}
 
-	const weatherImageURL = 'http://openweathermap.org/img/wn/'
-	const imageSize = '@2x.png'
-	//const time = moment.parseZone(props.current.dt*1000);
-	//const temp = convertKelvinToF(props.current.main.temp);
-	//const wind = meterPerSecToMPH(props.current.wind.speed);
-	//const humidity = props.current.main.humidity;
-	console.log(daily);
+	const weatherImageURL = 'http://openweathermap.org/img/wn/{iconid}@2x.png'; 
+	const indexes = [0,1,2,3,4,5,6,7];
+	const date =  moment.parseZone(daily[curIndex].dt*1000);   
+	const windSpeed = meterPerSecToMPH(daily[curIndex].wind_speed);
+	const precipitation = daily[curIndex].pop*100;
+	const humidity = daily[curIndex].humidity;
+	const dewPoint = convertKelvinToF(daily[curIndex].dew_point);
+
+	function handleSelection(index){
+		setCurIndex(index);
+	}
+
+
+
+	console.log(daily[curIndex]);
 
 	return (
 		<div className='daily-panel'>
+			<h1>Daily Forecast</h1>
 			<div className='daily-panel-weather-info'>
+				<p>{date.format('ll')}</p>
+				<img src={weatherImageURL.replace('{iconid}', daily[curIndex].weather[0].icon)}/>
+				<p className='daily-precipitation'>Preciptiation: {precipitation.toFixed(0)}%</p>
+				<p className='daily-wind-speed'>Wind Speed: {windSpeed.toFixed(0)}mph</p>
+				<p className='daily-humidity'>Humidity: {humidity}%</p>
+				<p className='daily-dew-point'>Dew Point: {dewPoint.toFixed(0)}</p>
+				<p className='daily-uvi'>UV Index: {daily[curIndex].uvi}</p>
 
 			</div>
+
 			<div className='daily-card-container'>
-				<DailyCard data={daily.daily[0]} index={0}/>
+				{indexes.map(i=>(
+					<DailyCard
+						key = {i}
+						data = {daily[i]}
+						isSelected = {i===curIndex}
+						onClick={() => setCurIndex(i)}
+					/>
+				))}
 			</div>
 		</div>
 
