77ffbdc47e781063c06181924fdbbd765ddd8741
breezycamel <phuocho99@gmail.com>
2020-10-08 14:04:51 -0400

    Updates

---
+++ precache-manifest.9b4f7e5f913f2fb4935eb1473f792bbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2950acbb633d49697130e0ca7b7aebe6",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "e24dc1b54b0efa2e64fd",
+    "url": "/weather-app/static/css/main.07cb28a2.chunk.css"
+  },
+  {
+    "revision": "025d34a5e9ebdcf05565",
+    "url": "/weather-app/static/js/2.abbf1a31.chunk.js"
+  },
+  {
+    "revision": "0fce063a4ec3cb005e7265693b407036",
+    "url": "/weather-app/static/js/2.abbf1a31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e24dc1b54b0efa2e64fd",
+    "url": "/weather-app/static/js/main.34292df4.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
